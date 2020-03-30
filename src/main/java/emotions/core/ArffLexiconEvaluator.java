8c0c9db27ef90c2ca32c1eceb96ee79449d3a228
dhanushmis18 <62444667+dhanushmis18@users.noreply.github.com>
2020-03-30 18:13:56 +0530

    Update ArffLexiconEvaluator.java

--- src/main/java/emotions/core/ArffLexiconEvaluator.java
+++ src/main/java/emotions/core/ArffLexiconEvaluator.java
@@ -1,24 +1,3 @@
-/*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- *    ArffLexiconEvaluator.java
- *    Copyright (C) 1999-2018 University of Waikato, Hamilton, New Zealand
- *
- */
-
 package affective.core;
 
 import java.io.BufferedReader;
@@ -44,16 +23,6 @@ import weka.core.stemmers.NullStemmer;
 import weka.core.stemmers.Stemmer;
 
 
-/**
- *  <!-- globalinfo-start --> 
- *  This class is used for calculating scores for a tweet using a list of affective lexicons in arff format. 
- *  Numeric associations are added  and nominal ones are counted.
- *  <!-- globalinfo-end -->
- * 
- * 
- * @author Felipe Bravo-Marquez (fbravoma@waikato.ac.nz)
- * @version $Revision: 1 $
- */
 public class ArffLexiconEvaluator implements Serializable, OptionHandler {
 
 	/** For serialization. */
