fe76f72d6f8aafcc62837317f088f478b2a5e144
dhanushmis18 <62444667+dhanushmis18@users.noreply.github.com>
2020-04-02 09:19:40 +0530

    Update IntensityLexiconEvaluator.java

--- src/main/java/emotions/core/IntensityLexiconEvaluator.java
+++ src/main/java/emotions/core/IntensityLexiconEvaluator.java
@@ -1,26 +1,3 @@
-/*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- *    IntensityLexiconEvaluator.java
- *    Copyright (C) 1999-2018 University of Waikato, Hamilton, New Zealand
- *
- */
-
-
-
 package affective.core;
 
 import java.io.BufferedReader;
@@ -33,16 +10,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
-
-/**
- *  <!-- globalinfo-start --> 
- *  This class is used for evaluating lexicons with numerical sentiment scores.
- *  <!-- globalinfo-end -->
- * 
- * 
- * @author Felipe Bravo-Marquez (fbravoma@waikato.ac.nz)
- * @version $Revision: 1 $
- */
+//This class is used for evaluating lexicons with numerical sentiment scores.
+ 
 public class IntensityLexiconEvaluator extends LexiconEvaluator  {
 
 
