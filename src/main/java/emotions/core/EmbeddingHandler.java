d2f7bf9ebdea5d3a08c8ee2bf34898c8d20636bf
dhanushmis18 <62444667+dhanushmis18@users.noreply.github.com>
2020-04-02 09:18:59 +0530

    Update EmbeddingHandler.java

--- src/main/java/emotions/core/EmbeddingHandler.java
+++ src/main/java/emotions/core/EmbeddingHandler.java
@@ -1,26 +1,3 @@
-/*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- *    EmbeddingHandler.java
- *    Copyright (C) 1999-2018 University of Waikato, Hamilton, New Zealand
- *
- */
-
-
-
 package affective.core;
 
 import it.unimi.dsi.fastutil.doubles.AbstractDoubleList;
@@ -33,19 +10,8 @@ import java.util.Enumeration;
 import weka.core.Option;
 import weka.core.OptionHandler;
 
+//This abstract class is used for handling word vector or embeddings.
 
-
-
-/**
- *  <!-- globalinfo-start --> 
- *  This abstract class is used for handling word vector or embeddings.
- * 
- * <!-- globalinfo-end -->
- * 
- * 
- * @author Felipe Bravo-Marquez (fbravoma@waikato.ac.nz)
- * @version $Revision: 1 $
- */
 public abstract class EmbeddingHandler implements Serializable, OptionHandler {
 
 
