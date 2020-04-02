1caf81b6bd5087afb1cb0e87ad21902de15184c9
dhanushmis18 <62444667+dhanushmis18@users.noreply.github.com>
2020-04-02 09:25:08 +0530

    Update SentiStrengthEvaluator.java

--- src/main/java/emotions/core/SentiStrengthEvaluator.java
+++ src/main/java/emotions/core/SentiStrengthEvaluator.java
@@ -1,26 +1,3 @@
-/*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- *    SentiStrengthEvaluator.java
- *    Copyright (C) 1999-2018 University of Waikato, Hamilton, New Zealand
- *
- */
-
-
-
 package affective.core;
 
 
@@ -32,16 +9,8 @@ import java.util.Map;
 
 import uk.ac.wlv.sentistrength.SentiStrength;
 
-
-/**
- *  <!-- globalinfo-start --> 
- *  This class is used for evaluating a tweet with SentiStrength.
- * <!-- globalinfo-end -->
- * 
- * 
- * @author Felipe Bravo-Marquez (fjb11@students.waikato.ac.nz)
- * @version $Revision: 1 $
- */
+//This class is used for evaluating a tweet with SentiStrength.
+ 
 public class SentiStrengthEvaluator extends LexiconEvaluator  {
 
 
