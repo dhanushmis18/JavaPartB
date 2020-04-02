7bb72f6d1a3f15322dc244354778f87d89b3defe
dhanushmis18 <62444667+dhanushmis18@users.noreply.github.com>
2020-04-02 09:18:24 +0530

    Update CSVEmbeddingHandler.java

--- src/main/java/emotions/core/CSVEmbeddingHandler.java
+++ src/main/java/emotions/core/CSVEmbeddingHandler.java
@@ -1,26 +1,3 @@
-/*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- *    EmbeddingHandler.java
- *    Copyright (C) 1999-2018 University of Waikato, Hamilton, New Zealand
- *
- */
-
-
-
 package affective.core;
 
 import it.unimi.dsi.fastutil.doubles.AbstractDoubleList;
@@ -36,17 +13,8 @@ import weka.core.OptionMetadata;
 import weka.core.SingleIndex;
 import weka.core.WekaPackageManager;
 
-
-/**
- *  <!-- globalinfo-start --> 
- *  This class is used for handling word vector or embeddings stored in gzipped files.
- * 
- * <!-- globalinfo-end -->
- * 
- * 
- * @author Felipe Bravo-Marquez (fbravoma@waikato.ac.nz)
- * @version $Revision: 1 $
- */
+//This class is used for handling word vector or embeddings stored in gzipped files.
+	 
 public class CSVEmbeddingHandler extends EmbeddingHandler {
 
 	/** For serialization **/ 
