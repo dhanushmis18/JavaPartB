eab570233db4a939718fc9f45c77d5ba077cff0b
dhanushmis18 <62444667+dhanushmis18@users.noreply.github.com>
2020-04-02 09:21:30 +0530

    Update LexiconEvaluator.java

--- src/main/java/emotions/core/LexiconEvaluator.java
+++ src/main/java/emotions/core/LexiconEvaluator.java
@@ -1,23 +1,3 @@
-/*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- *    LexiconEvaluator.java
- *    Copyright (C) 2018 University of Waikato, Hamilton, New Zealand
- */
-
 package affective.core;
 
 import java.io.IOException;
@@ -25,15 +5,8 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
-/**
- *  <!-- globalinfo-start --> 
- *  This abstract class provides the structure for all classes calculating attributes from lexicons.
- * <!-- globalinfo-end -->
- * 
- * 
- * @author Felipe Bravo-Marquez (fbravoma@waikato.ac.nz)
- * @version $Revision: 1 $
- */
+//This abstract class provides the structure for all classes calculating attributes from lexicons.
+ 
 public abstract class LexiconEvaluator implements Serializable {
 
 	/** for serialization */
