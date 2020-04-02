9990301066d5b2e5b581cae2f4910c311decdeaf
dhanushmis18 <62444667+dhanushmis18@users.noreply.github.com>
2020-04-02 09:31:50 +0530

    Update HumanCodedToArff.java

--- src/main/java/weka/core/converters/HumanCodedToArff.java
+++ src/main/java/weka/core/converters/HumanCodedToArff.java
@@ -1,25 +1,3 @@
-/*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- *    HumanCodedToArff.java
- *    Copyright (C) 1999-2018 University of Waikato, Hamilton, New Zealand
- *
- */
-
-
 package weka.core.converters;
 
 import java.io.BufferedReader;
@@ -33,13 +11,8 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 
-/**
- * Builds an arff dataset from the 6HumanCoded collection of tweets for sentiment
- * analysis. 
- * 
- * @author Felipe Bravo-Marquez (fbravoma@waikato.ac.nz)
- * @version 1.0
- */
+//Builds an arff dataset from the 6HumanCoded collection of tweets for sentiment analysis. 
+
 public class HumanCodedToArff extends TweetCollectionToArff {
 
 	/* (non-Javadoc)
