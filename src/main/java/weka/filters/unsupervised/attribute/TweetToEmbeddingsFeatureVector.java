dae92a05c9f0515760a08a20b62ca43e86734c48
dhanushmis18 <62444667+dhanushmis18@users.noreply.github.com>
2020-04-02 09:51:17 +0530

    Update TweetToEmbeddingsFeatureVector.java

--- src/main/java/weka/filters/unsupervised/attribute/TweetToEmbeddingsFeatureVector.java
+++ src/main/java/weka/filters/unsupervised/attribute/TweetToEmbeddingsFeatureVector.java
@@ -1,25 +1,3 @@
-/*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- *    TweetToEmbeddingsFeatureVector.java
- *    Copyright (C) 1999-2018 University of Waikato, Hamilton, New Zealand
- *
- */
-
-
 package weka.filters.unsupervised.attribute;
 
 import java.io.File;
@@ -37,34 +15,6 @@ import weka.core.SparseInstance;
 import weka.core.TechnicalInformation;
 import weka.core.WekaPackageManager;
 import weka.core.TechnicalInformation.Type;
-
-
-/**
- *  <!-- globalinfo-start --> An attribute filter that calculates word embedding (word vectors) features 
- *  for a tweet represented as a string attribute. 
- * <!-- globalinfo-end -->
- * 
- * <!-- technical-bibtex-start -->
- * BibTeX:
- * <pre>
- * &#64;inproceedings{bravo-marquez16:_deter_word_emotion_assoc_from,
- * author = {Felipe Bravo-Marquez and Eibe Frank and Saif M. Mohammad and Bernhard Pfahringer},
- * title = {Determining Word-Emotion Associations from Tweets by Multi-Label Classification},
- * booktitle = {Proc 15th IEEE/WIC/ACM International Conference on Web Intelligence},
- * year = 2016,
- * series = {Omaha, Nebraska},
- * publisher = {IEEE Computer Society},
- * pdf = {http://www.cs.waikato.ac.nz/~eibe/pubs/emo_lex_wi.pdf}
- * }
- * </pre>
- <!-- technical-bibtex-end -->
- * 
- * 
- * 
- * @author Felipe Bravo-Marquez (fbravoma@waikato.ac.nz)
- * @version $Revision: 2 $
- */
-
 
 public class TweetToEmbeddingsFeatureVector extends TweetToFeatureVector {
 
